5c4a7ec9a778a94a03b598e5d830c161f5f12359
onochie ndiichie <ifedebe.o@gmail.com>
2022-12-09 19:25:17 -0600

    initial Commit

---
+++ src/main/java/com/jandk/protaskmanager/ProTaskManagerApplication.java
@@ -0,0 +1,13 @@
+package com.jandk.protaskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProTaskManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProTaskManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jandk/protaskmanager/ProTaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jandk.protaskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProTaskManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
